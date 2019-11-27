0e2294cf35eb9abd30ebe99c69118ae0d7a3a802
rogeriodesaf <rogeriodesaf@gmail.com>
2019-11-27 19:03:20 -0300

    Validando busca pelo CPF

--- portaria/index.php
+++ portaria/index.php
@@ -18,14 +18,23 @@
  </div><!-- porteiro -->
  
  <div id="logo">
-  <img src="../img/STCN.png" width="250px" />
+  <img src="../img/STCN.png" width="400px" />
  </div><!-- logo -->
  
  <div id="campo_busca">
    <form name="" method="post" action="" enctype="multipart/form-data">
    <input type="text" name="cpf" value="" /><input class="input" type="submit" name="send" value="" />
   </form>
-  
+  <?php  
+    if(isset($_POST['send'])){  //se o botão send for pressionado ele vai criar uma variável chamada cpf que vai receber o que vier do campo CPF. 
+         $cpf = $_POST['cpf'];
+        
+    if($cpf == ''){      //se o meu cpf for vazio.
+            echo "<script language='javascript'> window.alert ('Por favor, digite o seu cpf!'); </script>";
+
+        }  
+    }
+?>
 
 <br><br><br><br><h3><strong>Aluno:</strong>  <strong>Nº de matricula:</strong> <strong>RG:</strong>  <a href="index.php?pg=confirma&code_a=<img src="../img/correto.jpg" title="Confirmar" border="0" /></a> <a href="index.php"><img src="img/delete.jpg" width="24px" title="Cancelar" /></a> </h3><input type="hidden" name="codes" value="" />  
  
