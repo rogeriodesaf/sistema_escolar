ba01e76a3e44a29a99fa331ec8f8ad7373b056f2
rogeriodesaf <rogeriodesaf@gmail.com>
2019-12-03 15:42:34 -0300

    Começando a trabalhar na página do Painel

--- admin/topo.php
+++ admin/topo.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<? require "../config.php"; ?>
+ <? require "../config.php"; ?>  <!-- Requie          -->
 <link href="css/topo.css" rel="stylesheet" type="text/css" />
 <script language="javascript" src="../js/jquery-1.7.2.min.js"></script>
 <script src="../js/lightbox.js"></script>
@@ -41,7 +41,7 @@
 <div id="box_topo">
  
  <div id="logo">
-  <img src="../img/logo.png" width="250"/>
+  <img src="../img/STCN.png" width="400"/>
  </div><!-- logo -->
  
  <div id="campo_busca">
