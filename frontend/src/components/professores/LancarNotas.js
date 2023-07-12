9f38b5f1f7bd87cb957a2d80e26c5c34e1a582f8
Rogério de Sá <rogeriodesaf@gmail.com>
2023-07-11 23:46:54 -0300

    Reparos no arquivo 'LancarPresenca.js

--- frontend/src/components/professores/LancarNotas.js
+++ frontend/src/components/professores/LancarNotas.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import useFlashMessage from '../../hooks/useFlashMessage'
+import Input from '../forms/Input'
+import styles from '../forms/Form.module.css'
+import stylesButton from '../professores/RegistrarAula.module.css';
 
 
 const LancarNotas = () => {
  
-
+  const [alunoId, setAlunoId] = useState('');
+  const [nota, setNota] = useState('');
   const { disciplinaId } = useParams();
   const [alunos, setAlunos] = useState([]);
-  const [notas, setNotas] = useState({});
+  const [notas, setNotas] = useState([]);
+  const { setFlashMessage } = useFlashMessage();
 
 
 
@@ -27,37 +33,55 @@ const LancarNotas = () => {
   }, [disciplinaId]);
 
   const handleNotaChange = (alunoId, nota) => {
-    setNotas((prevNotas) => ({
-      ...prevNotas,
-      [alunoId]: nota
-    }));
+    setNotas((prevNotas)=>(
+      {
+        ...prevNotas,
+        [alunoId]:nota,
+      }
+    ));
+   // console.log('alunoId:', alunoId);
+   // console.log('nota:', nota);
+    
+    //setAlunoId(alunoId);
+   // setNota(nota);
+
+    
+  
+
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    let msgText = 'Nota registrada com sucesso para esse aluno!';
+    let msgType = 'sucess';
+     
     try {
-      const notasData = {
-        notas: notas
-      };
+      
+     
+      for (const alunoId in notas) {
+        const nota = notas[alunoId];
+        await axios.post(`http://localhost:5000/api/notas/${disciplinaId}/aluno/${alunoId}`, { nota });
+      }
+        
 
-      await axios.post(`http://localhost:5000/api/notas/${disciplinaId}`, notasData);
-
-      console.log('Notas lançadas com sucesso!');
-      limparCampos();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+     console.log('Notas enviadas com sucesso!');
+    // Limpar os valores após o envio, se necessário
+    limparCampos()
+  } catch (error) {
+    console.error('Erro ao enviar as notas:', error);
+  }
+  setFlashMessage(msgText, msgType);
+};
 
   const limparCampos = () => {
-    setNotas({});
+    console.log('limpando campos')
+   
   };
 
   return (
     <div>
       <h1>Lançar Notas</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.form_container}>
         <table>
           <thead>
             <tr>
@@ -70,17 +94,18 @@ const LancarNotas = () => {
               <tr key={aluno._id}>
                 <td>{aluno.firstName} {aluno.lastName}</td>
                 <td>
-                  <input
+                  <Input
                     type="number"
-                    value={notas[aluno._id] || ''}
+                    defaultValue={notas[aluno._id] || ''}
                     onChange={(e) => handleNotaChange(aluno._id, e.target.value)}
+                    placeholder='Insira aqui a nota do aluno!'
                   />
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
-        <button type="submit">Lançar Notas</button>
+        <button className={stylesButton.formButton} onClick={limparCampos} type="submit">Lançar Notas</button>
       </form>
     </div>
   );
